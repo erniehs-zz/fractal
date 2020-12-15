21a1892117d880c7acb917e8eb4993d35b302d1c
ernie_hs <ernie_hs@hotmail.com>
2020-12-15 08:24:53 +0000

    tidy up

--- fractal.js
+++ fractal.js
@@ -1,14 +1,25 @@
+const R = 1.6; // roughness
+const DEPTH = 9; // size
+const PTS = Math.pow(2, DEPTH) + 1; // total points
+const DH = 60; // height/blob size
+const SZ = 2; // block size
+const Y = 400; // zero level, start of draw
+
+var frac = _dim([PTS]); // fractal manifold...
+
 const _mod = (x, y) => Math.abs(x % y);
 
 const _randn = () => Math.random() - 0.5; // -0.5 to 0.5, more or less...
 
-const _mid = (v) => v.reduce((a, c) => c.map((n, i) => n + (a[i] - n) / 2)); 
+const _mid = (v) => v.reduce((a, c) => c.map((n, i) => n + (a[i] - n) / 2));
 
-const _scale = (n) => Math.exp(-n / 1.7);
+const _scale = (n) => Math.exp(-n / R);
 
 const _midp = (p1, p2) => Math.min(p1, p2) + Math.abs((p2 - p1) / 2);
 
 const _subdivide = (i, frac, v1, v2, dh) => {
+
+  // TODO fill the void...  v1/v2 defines n-dimensional hypercube...
   const dv = _sub(v2, v1);
   if (_dot(dv, dv) == frac.dims.length) return;
   const mv = _mid([v1, v2]);
@@ -21,16 +32,9 @@ const _landscape = (frac, dh) => {
   frac.set([[0]], _randn() * dh);
   frac.set([frac.dims[0] - 1], _randn() * dh);
   _subdivide(0, frac, [0], [frac.dims[0] - 1], dh);
-}
+};
 
-const DH = 50.0;
-const DEPTH = 9;
-const PTS = Math.pow(2, DEPTH) + 1;
-var frac = _dim([PTS]);
-_landscape(frac, DH);
-
-const SZ = 2;
-const Y = 600;
+_landscape(frac, DH); // generate the fractal
 
 const draw = () => {
   ctx.fillStyle = "cornflowerblue";
